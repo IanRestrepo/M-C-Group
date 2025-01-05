f2ff4f26ee7585981778a90171b638114a8ecd2a
Ian Restrepo <ianrestrepog@gmail.com>
2025-01-05 15:56:51 -0500

    FN

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-  mode: 'standalone'
-}),
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   integrations: [tailwind(), icon()]
 });
